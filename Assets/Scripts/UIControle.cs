0d552e4ca046ca867c666d9b7ecd420f0974d7c4
Lucas Corrêa <Lucas Corra>
2017-04-22 19:25:04 -0300

    unity prt

---
+++ Assets/Scripts/UIControle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControle : MonoBehaviour {
+
+    public void iniciarJogo(string nomeCena)
+    {
+        Application.LoadLevel(nomeCena);
+    }
+
+}
